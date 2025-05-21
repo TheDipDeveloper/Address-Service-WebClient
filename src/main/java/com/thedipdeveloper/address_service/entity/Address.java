6ebbc7472c33fecdaedd63d73c16047c84553790
TheDipDeveloper <dipdeveloper.connect@gmail.com>
2025-05-21 22:12:33 +0530

    address service

---
+++ src/main/java/com/thedipdeveloper/address_service/entity/Address.java
@@ -0,0 +1,50 @@
+package com.thedipdeveloper.address_service.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "address")
+public class Address {
+
+    @Id
+    private int id;
+
+    private String city;
+
+    private String state;
+
+    private int empId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+}
+
